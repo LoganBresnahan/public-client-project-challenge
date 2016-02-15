260c7677b6ad02f6bbc23d896bb09a43ad4e3383
Caroline Kenworthy <email": null>
2016-02-15 16:01:50 -0600

    Add simplecov setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def does_not_suck?
+    return true
+  end
+
 end
