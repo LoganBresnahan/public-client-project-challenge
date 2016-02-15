5938404e820becfb2082d72b3b9a52f0593e0f39
Logan Bresnahan & Steven Jones <pair+loganbresnahan.sjones88@devbootcamp.com>
2016-02-15 17:33:23 -0600

    Add resources to sessions.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'games#index'
   resources :users
+  resources :sessions, only: [:create, :new, :destroy]
 
 
   # Example of regular route:
