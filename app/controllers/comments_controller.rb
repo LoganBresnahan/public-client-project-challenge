979dda2919cca1c9333f3ae0c3c3b9577e562324
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 15:56:26 -0600

    Add comment post to controller and form to game/show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,23 @@ class CommentsController < ApplicationController
   end
 
   def create
+    p params
+    p "*********************"
+    current_user = User.find(session[:user_id])
+    game = Game.find(params[:game_id])
+    @comment = current_user.comments.build(comment_params)
+    @comment.game = game
+    if @comment.save
+      redirect_to game_path(game)
+    else
+      flash[:alert] = "Comment did not save."
+      redirect_to game_path(game)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
