e87eecd66f1d734b73a46313cc1438201c8b03b2
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 11:26:32 -0600

    Add flash notice and create action to stashes controller

--- app/controllers/stashes_controller.rb
+++ app/controllers/stashes_controller.rb
@@ -9,7 +9,11 @@ class StashesController < ApplicationController
   def create
     user = User.find(params[:user_id])
     game = Game.find(params[:game_id]) # Need to clarify what params will be called
-    user.games << game
+    if !user.games.find_by(id: game.id)
+      user.games << game
+    else
+      flash[:alert] = "Game already exists in your collection"
+    end
     redirect_to games_path
   end
 
