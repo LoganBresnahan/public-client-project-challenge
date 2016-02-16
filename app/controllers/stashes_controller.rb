c79a0f985491ebada067929f90cf281fe6b33182
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 11:07:26 -0600

    Add ability to add game to user's stash

--- app/controllers/stashes_controller.rb
+++ app/controllers/stashes_controller.rb
@@ -1,13 +1,16 @@
-class StashController < ApplicationController
+class StashesController < ApplicationController
   def index
     @stashes = Stash.all
+  end
+
+  def new
   end
 
   def create
     user = User.find(params[:user_id])
     game = Game.find(params[:game_id]) # Need to clarify what params will be called
     user.games << game
-    redirect_to user_path(user)
+    redirect_to games_path
   end
 
   def destroy
