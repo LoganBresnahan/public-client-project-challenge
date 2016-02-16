8c3d14a54394fba3d2fab2b4b0141890926678c6
Caroline Kenworthy & Steven Jones <pair+kenworthyc.sjones88@devbootcamp.com>
2016-02-16 17:23:26 -0600

    Add remove friends function

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -8,4 +8,10 @@ class FriendshipsController < ApplicationController
     user.friends << friend
     redirect_to user_path(friend)
   end
+
+  def destroy
+    burn_bridge = Friendship.find(params[:id])
+    burn_bridge.destroy
+    redirect_to user_path(session[:user_id])
+  end
 end
