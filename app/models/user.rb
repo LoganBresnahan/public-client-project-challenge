e57ef57dd9e42358dd3988b4c99401e6e2ffb4d7
Caroline Kenworthy & Steven Jones <pair+kenworthyc.sjones88@devbootcamp.com>
2016-02-16 14:22:32 -0600

    Add ability to add friendships to groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :games, through: :stashes
   has_many :friendships
   has_many :groups, foreign_key: :creator_id
-  has_many :friends, through: :friendships, source: :user
+  has_many :friends, through: :friendships, source: :friend
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
