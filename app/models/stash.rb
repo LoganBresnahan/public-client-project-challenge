a2ee4853800f2dc92989017381bb029ad8e8d209
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 11:28:41 -0600

    Add form for creating a new stash and update games index with add button.

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -1,4 +1,4 @@
-class Stash < ActiveRecord::Base
+dd class Stash < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
   validates :game, uniqueness: { scope: :user, message: "cannot be added twice to your games" }
