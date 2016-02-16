b394f929ca97c4949ecd7b3ec478b16394044ed3
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 08:59:54 -0600

    Add associations for collection

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :games_categories
   has_many :categories, through: :games_categories
+  has_many :collections
+  has_many :users, through: :collections
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :collections
+  has_many :games, through: :collections
+
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
