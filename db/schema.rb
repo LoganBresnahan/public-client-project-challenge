3a70eb74cbe24902576e1bab09f93c41f9ab540d
Caroline Kenworthy <email": null>
2016-02-15 18:54:46 -0600

    Add models, migrations, spec file for category

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216002350) do
+ActiveRecord::Schema.define(version: 20160216005314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "bgg_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "categories", ["name"], name: "index_categories_on_name", using: :btree
 
   create_table "games", force: :cascade do |t|
     t.string   "name",           null: false
