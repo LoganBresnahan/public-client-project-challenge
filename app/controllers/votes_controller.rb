6154eef04b1719091f1bea72bd11a77ada9ae902
dma315 <dma315@gmail.com>
2016-02-17 10:11:41 -0600

    DRY up voting by extracting to helper module

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,35 +1,16 @@
 class VotesController < ApplicationController
   
+  include VotingHelper
 
-
-  # Not DRY, needs refactoring
   def upvote
     @game = Game.find_by(id: params[:id])
-    vote = current_user.votes.new(game: @game, value: 1)
-    if vote.save
-      @new_score = @game.vote_score
-      respond_to do |format|
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.js {render nothing: true}
-      end
-    end
+    @vote = current_user.votes.new(game: @game, value: 1)
+    respond_to_voting
   end
 
   def downvote
     @game = Game.find_by(id: params[:id])
-    vote = current_user.votes.new(game: @game, value: -1)
-    if vote.save
-      @new_score = @game.vote_score
-      respond_to do |format|
-        format.js
-      end
-    else
-      respond_to do |format|
-        format.js {render nothing: true}
-      end
-    end
+    @vote = current_user.votes.new(game: @game, value: -1)
+    respond_to_voting
   end
 end
