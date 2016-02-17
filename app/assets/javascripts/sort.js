248cd4d48ec1d97337ef624bf533dabb5e052009
David Ma <email": null>
2016-02-17 11:26:58 -0600

    Add sort functionality to games

---
+++ app/assets/javascripts/sort.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $(".sort-panel").on('click', '.btn', function(event) {
+    event.preventDefault()
+    var sortMethod = $(this).text()
+    var $gameCards = []
+    $(".game-card").each(function(index, element) {
+      $gameCards.push(element)
+    })
+    $gameCards.sort(methods[sortMethod]);
+    for (var i in $gameCards) {
+      $('#game-container').append($gameCards[i])
+    }
+  })
+
+  var methods = {
+    "A-Z": function(a, b) {
+      if ($(a).find("h4 a").text() < $(b).find("h4 a").text()) return -1
+      if ($(b).find("h4 a").text() < $(a).find("h4 a").text()) return 1
+      return 0
+    },
+    "Z-A": function(a, b) {
+      if ($(b).find("h4 a").text() < $(a).find("h4 a").text()) return -1
+      if ($(a).find("h4 a").text() < $(b).find("h4 a").text()) return 1
+      return 0
+    },
+    "Highest Rated": function(a, b) {
+      return $(b).find(".vote-score").text() - $(a).find(".vote-score").text()
+    }
+  }
+
+})
