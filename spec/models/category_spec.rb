aded1072b877795c0401b2fa4782915b6f984d23
Caroline <kenworthyc@gmail.com>
2016-02-16 19:10:18 -0600

    Add category specs

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
+# require 'spec_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # let!(:arkham) { Game.create!(name: "Arkham Horror", description: "spooky") }
+  # let!(:mystery) { Category.create!(name: "Mystery", bgg_id: 14) }
+  # let!(:categorization) { GamesCategory.create!(game_id: arkham.id, category_id: 14) }
+  before(:each) do
+    @game = Game.create!(name: "Arkham Horror", description: "spooky")
+    @category = Category.create!(name: "Mystery", bgg_id: 14)
+    @link = GamesCategory.create!(game_id: @game.id, category_id: @category.bgg_id)
+  end
+
+  it "has a name" do
+    expect(@category.name).to eq "Mystery"
+  end
+
+  xit "has an associated game" do
+    expect(@category.games).to eq @game
+  end
 end
