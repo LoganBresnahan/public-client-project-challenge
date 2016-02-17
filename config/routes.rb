44091449bbddbb7a38192a1ea638c4b4c9c3a519
David Ma & Logan Bresnahan <pair+dma315.loganbresnahan@devbootcamp.com>
2016-02-16 19:02:58 -0600

    Add categories controller and update routes file and games show.

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,9 @@ Rails.application.routes.draw do
 
   resources :games do
     resources :comments, only: [:index, :create, :destroy]
+    resources :categories, only: [:index, :show]
   end
+
 
 
   # Example of regular route:
